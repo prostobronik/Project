8402537fe391eb9e02955bd0aaabdf364a0bb094
Nikita <nik.nekit.14@mail.ru>
2022-10-08 15:33:33 +0000

    Исправил баг с датой и удалил лишнюю строку

--- local/templates/home/footer.php
+++ local/templates/home/footer.php
@@ -54,7 +54,7 @@
             
 
           </div>
-          <? use Bitrix\Main\Type\DateTime;?>
+         
         </div>
         <div class="row pt-5 mt-5 text-center">
           <div class="col-md-12">
